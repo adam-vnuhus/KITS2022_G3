64294566a25d696affdc586877342dad34a9057c
14ThuHang <14thuhang@gmail.com>
2022-08-09 09:09:26 +0700

    pom

---
+++ be-Project_eMarket_G3/src/main/java/com/example/Project_eMarket_G3/ProjectEMarketG3Application.java
@@ -0,0 +1,13 @@
+package com.example.Project_eMarket_G3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEMarketG3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectEMarketG3Application.class, args);
+	}
+
+}
---
+++ be-Project_eMarket_G3/src/test/java/com/example/Project_eMarket_G3/ProjectEMarketG3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Project_eMarket_G3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectEMarketG3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
