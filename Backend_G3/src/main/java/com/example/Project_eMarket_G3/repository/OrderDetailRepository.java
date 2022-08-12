b061c88a011dd09f3cc80f515c80c4d5e760b3f9
nqm0711 <qm0711@gmail.com>
2022-08-12 12:11:05 +0700

    Update

--- Backend_G3/src/main/java/com/example/Project_eMarket_G3/repository/OrderDetailRepository.java
+++ Backend_G3/src/main/java/com/example/Project_eMarket_G3/repository/OrderDetailRepository.java
@@ -5,9 +5,13 @@ import com.example.Project_eMarket_G3.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 
 public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
     long countByProduct_Id(Long id);
+
+    List<OrderDetail> getOrderDetailsByOrderId(Long orderID);
 
 }
