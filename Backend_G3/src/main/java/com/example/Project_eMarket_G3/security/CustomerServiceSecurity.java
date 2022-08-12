e8cc09f042bfac8d16f99538b452b54fae23ce83
14ThuHang <14thuhang@gmail.com>
2022-08-12 11:57:18 +0700

    form register

---
+++ Backend_G3/src/main/java/com/example/Project_eMarket_G3/security/CustomerServiceSecurity.java
@@ -0,0 +1,8 @@
+package com.example.Project_eMarket_G3.security;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerServiceSecurity extends CustomerDetailSecurity {
+
+}
