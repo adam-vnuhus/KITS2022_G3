e2c307d934a3bd9d5c971ab1be3e59738ba05913
14ThuHang <14thuhang@gmail.com>
2022-08-12 08:55:01 +0700

    test api

---
+++ Backend_G3/src/main/java/com/example/Project_eMarket_G3/controller/OrderController.java
@@ -0,0 +1,35 @@
+package com.example.Project_eMarket_G3.controller;
+
+import com.example.Project_eMarket_G3.entity.OrderDetail;
+import com.example.Project_eMarket_G3.entity.Orders;
+import com.example.Project_eMarket_G3.repository.OrderDetailRepository;
+import com.example.Project_eMarket_G3.repository.OrdersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+
+    @Autowired
+    private OrdersRepository ordersRepository;
+    @Autowired
+    private OrderDetailRepository orderDetailRepository;
+
+
+    @GetMapping
+    public List<Orders> getAllOrder(){
+        return ordersRepository.findAll();
+    }
+
+    @GetMapping("/details")
+    public List<OrderDetail> getAllDetail(){
+        return orderDetailRepository.findAll();
+    }
+
+}
