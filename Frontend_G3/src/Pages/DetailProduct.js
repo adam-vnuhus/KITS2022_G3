14487d274ad90057c53d2f874ed6a0d6d6fbd3c8
momongako <kuuhaku771998@gmail.com>
2022-08-15 19:00:37 +0700

    ok

--- Frontend_G3/src/Pages/DetailProduct.js
+++ Frontend_G3/src/Pages/DetailProduct.js
@@ -178,6 +178,76 @@ const DetailProduct = () => {
 
             {/* end top product */}
 
+            <div>
+                <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" />
+                <div className="container">
+                    <div className="be-comment-block">
+                        <h1 className="comments-title">Comments (3)</h1>
+                        <div className="be-comment">
+                            <div className="be-img-comment">
+                                <a href="blog-detail-2.html">
+                                    <img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="" className="be-ava-comment" />
+                                </a>
+                            </div>
+                            <div className="be-comment-content">
+                                <span className="be-comment-name">
+                                    <a href="blog-detail-2.html">Ravi Sah</a>
+                                </span>
+                                <span className="be-comment-time">
+                                    <i className="fa fa-clock-o" />
+                                    May 27, 2015 at 3:14am
+                                </span>
+                                <p className="be-comment-text">
+                                    Pellentesque gravida tristique ultrices.
+                                    Sed blandit varius mauris, vel volutpat urna hendrerit id.
+                                    Curabitur rutrum dolor gravida turpis tristique efficitur.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="be-comment">
+                            <div className="be-img-comment">
+                                <a href="blog-detail-2.html">
+                                    <img src="https://bootdey.com/img/Content/avatar/avatar2.png" alt="" className="be-ava-comment" />
+                                </a>
+                            </div>
+                            <div className="be-comment-content">
+                                <span className="be-comment-name">
+                                    <a href="blog-detail-2.html">Phoenix, the Creative Studio</a>
+                                </span>
+                                <span className="be-comment-time">
+                                    <i className="fa fa-clock-o" />
+                                    May 27, 2015 at 3:14am
+                                </span>
+                                <p className="be-comment-text">
+                                    Nunc ornare sed dolor sed mattis. In scelerisque dui a arcu mattis, at maximus eros commodo. Cras magna nunc, cursus lobortis luctus at, sollicitudin vel neque. Duis eleifend lorem non ant. Proin ut ornare lectus, vel eleifend est. Fusce hendrerit dui in turpis tristique blandit.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="be-comment">
+                            <div className="be-img-comment">
+                                <a href="blog-detail-2.html">
+                                    <img src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="" className="be-ava-comment" />
+                                </a>
+                            </div>
+                            {/*  comment,time */}
+                            <div className="be-comment-content">
+                                <span className="be-comment-name">
+                                    <a href="blog-detail-2.html">Cüneyt ŞEN</a>
+                                </span>
+                                <span className="be-comment-time">
+                                    <i className="fa fa-clock-o" />
+                                    May 27, 2015 at 3:14am
+                                </span>
+                                <p className="be-comment-text">
+                                    Cras magna nunc, cursus lobortis luctus at, sollicitudin vel neque. Duis eleifend lorem non ant
+                                </p>
+                            </div>
+                            {/* end  comment */}
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
