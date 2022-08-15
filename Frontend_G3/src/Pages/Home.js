6595fc1751a7127b99dc19a1eb7d7090ef5e75ff
momongako <kuuhaku771998@gmail.com>
2022-08-15 19:09:53 +0700

    ok

--- Frontend_G3/src/Pages/Home.js
+++ Frontend_G3/src/Pages/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <>
 
-            <div >categories component</div>
+
 
             {/* Featured Section Begin */}
             <section className="featured spad">
