6223e351a4946283abb03a07ec7531b61ef1fc28
momongako <kuuhaku771998@gmail.com>
2022-08-15 11:42:33 +0700

    layout update

--- Frontend_G3/src/App.js
+++ Frontend_G3/src/App.js
@@ -7,17 +7,30 @@ import Footer from './Components/Footer';
 import Home from './Pages/Home';
 import Categories from './Pages/HomeComponents/Categories';
 import ShopMainPage from "./Pages/ShopMainPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './Components/Layout';
 
 function App() {
   return (
-    <>
-      <Header />
-      <Home />
-      {/* <ShopMainPage /> */}
-      <Footer />
-      {/* <Categories /> */}
-      {/* error  library */}
-    </>
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          {/* <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    // <>
+    //   <Header />
+    //   <Home />
+    //   {/* <ShopMainPage /> */}
+    //   <Footer />
+    //   {/* <Categories /> */}
+    //   {/* error  library */}
+    // </>
   );
 }
 
