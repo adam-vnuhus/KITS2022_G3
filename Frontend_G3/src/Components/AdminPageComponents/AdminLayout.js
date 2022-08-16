9afbc45582325c31978760aedfa560482051ed6c
nqm0711 <qm0711@gmail.com>
2022-08-16 14:32:03 +0700

    Update AdminLayout.js

--- Frontend_G3/src/Components/AdminPageComponents/AdminLayout.js
+++ Frontend_G3/src/Components/AdminPageComponents/AdminLayout.js
@@ -10,7 +10,7 @@ const AdminLayout = () => {
             <AdminSideBar/>
                     <Outlet />
             </div>
-            <AdminFooter />
+            {/*<AdminFooter />*/}
     </>
 
         )
