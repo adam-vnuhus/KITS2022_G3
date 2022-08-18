92fe236918fd20f6187c16c9562cfdda1788db34
momongako <kuuhaku771998@gmail.com>
2022-08-18 15:24:47 +0700

    Merge branch 'main' of https://github.com/ThuHang14/KITS2022_G3

--- Frontend_G3/src/Pages/Home.js
+++ Frontend_G3/src/Pages/Home.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import Categories from './HomeComponents/Categories';
-
+import { useEffect, useState } from 'react';
+import ProductService from '../services/ProductService';
+import axios from 'axios';
 const Home = () => {
+    const [product, setProduct] = useState([]);
+
+    useEffect(() => {
+        ProductService.getProduct()
+            .then(response => response.data)
+            .then((data) => {
+                if (data.length > 0) {
+                    setProduct(data)
+                }
+            });
+
+    }, [])
+    console.log('>>> check dataProduct : ', product);
     return (
         <>
 
@@ -21,36 +36,46 @@ const Home = () => {
                 </div>
                 <div className="container">
                     <div className="row">
-                        <div className="col-md-6 col-lg-3 ">
-                            <div className="product">
-                                <a href="/" className="img-prod"><img className="img-fluid" src={require(`../img/product-1.jpg`)} alt="Colorlib Template" />
-                                    <span className="status">30%</span>
-                                    <div className="overlay" />
-                                </a>
-                                <div className="text py-3 pb-4 px-3 text-center">
-                                    <h3><a href="/">Bell Pepper</a></h3>
-                                    <div className="d-flex">
-                                        <div className="pricing">
-                                            <p className="price"><span className="mr-2 price-dc">$120.00</span><span className="price-sale">$80.00</span></p>
+
+                        {product && product.length > 0 ?
+                            product.map((item, index) => {
+                                return (
+                                    <div className="col-md-6 col-lg-3 ">
+                                        <div className="product">
+                                            <a href="/" className="img-prod"><img className="img-fluid" src={require(`../img/product-1.jpg`)} alt="Colorlib Template" />
+                                                <span className="status">30%</span>
+                                                <div className="overlay" />
+                                            </a>
+                                            <div className="text py-3 pb-4 px-3 text-center">
+                                                <h3><a href="/">{item.name}</a></h3>
+                                                <div className="d-flex">
+                                                    <div className="pricing">
+                                                        <p className="price"><span className="mr-2 price-dc">$120.00</span><span className="price-sale">${item.buyPrice}</span></p>
+                                                    </div>
+                                                </div>
+                                                <div className="bottom-area d-flex px-3">
+                                                    <div className="m-auto d-flex">
+                                                        <a href="/" className="add-to-cart d-flex justify-content-center align-items-center text-center">
+                                                            <span><i class="fa-solid fa-bars"></i></span>
+                                                        </a>
+                                                        <a href="/" className="buy-now d-flex justify-content-center align-items-center mx-1">
+                                                            <span><i class="fa-solid fa-cart-shopping"></i></span>
+                                                        </a>
+                                                        <a href="/" className="heart d-flex justify-content-center align-items-center ">
+                                                            <span><i class="fa-solid fa-heart"></i></span>
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
-                                    <div className="bottom-area d-flex px-3">
-                                        <div className="m-auto d-flex">
-                                            <a href="/" className="add-to-cart d-flex justify-content-center align-items-center text-center">
-                                                <span><i class="fa-solid fa-bars"></i></span>
-                                            </a>
-                                            <a href="/" className="buy-now d-flex justify-content-center align-items-center mx-1">
-                                                <span><i class="fa-solid fa-cart-shopping"></i></span>
-                                            </a>
-                                            <a href="/" className="heart d-flex justify-content-center align-items-center ">
-                                                <span><i class="fa-solid fa-heart"></i></span>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-6 col-lg-3 ">
+                                )
+                            })
+
+                            : 'loading'
+                        }
+
+                        {/* <div className="col-md-6 col-lg-3 ">
                             <div className="product">
                                 <a href="/" className="img-prod"><img className="img-fluid" src={require(`../img/product-2.jpg`)} alt="Colorlib Template" />
                                     <div className="overlay" />
@@ -246,7 +271,7 @@ const Home = () => {
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </section>
