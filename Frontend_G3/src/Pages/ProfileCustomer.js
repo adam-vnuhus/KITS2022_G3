30c2ee58ea5ffac2c193d23ea48b093a50857676
momongako <kuuhaku771998@gmail.com>
2022-08-26 10:29:03 +0700

    Update ProfileCustomer.js

--- Frontend_G3/src/Pages/ProfileCustomer.js
+++ Frontend_G3/src/Pages/ProfileCustomer.js
@@ -64,7 +64,7 @@ const ProfileCustomer = () => {
                                             <div className="card mb-4">
                                                 <div className="card-body text-center">
                                                     <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="avatar" className="rounded-circle img-fluid" style={{ width: '150px' }} />
-                                                    <h5 className="my-3">John Smith</h5>
+                                                    <h5 className="my-3">Nguyễn Văn Hậu</h5>
                                                     <p className="text-muted mb-1">Full Stack Developer</p>
                                                     <p className="text-muted mb-4">Bay Area, San Francisco, CA</p>
                                                     <div className="d-flex justify-content-center mb-2">
