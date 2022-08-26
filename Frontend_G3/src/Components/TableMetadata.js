e3b6c8bc30fea7fe0c2fd2e88e545ebb7a691ab1
nqm0711 <qm0711@gmail.com>
2022-08-26 10:13:34 +0700

    Not Ok

--- Frontend_G3/src/Components/TableMetadata.js
+++ Frontend_G3/src/Components/TableMetadata.js
@@ -23,7 +23,7 @@ export default function TableMetadata({table, setProps}) {
             break;
         case "user":
             setProps({
-                entity: 'product',
+                entity: 'User',
                 linkField: 'http://localhost:8080/api/v1/metadata/User/variables',
                 linkAPI: 'http://localhost:8080/api/admin/user',
                 searchLink: 'http://localhost:8080/api/admin/user',
